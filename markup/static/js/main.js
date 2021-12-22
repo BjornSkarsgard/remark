fce791c528ab12f4f0684239b0139f87eca92a21
unknown <bjornskarsgarddev@gmail.com>
2021-12-22 19:30:02 +0500

    ready

---
+++ markup/static/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+import "./libraries/lib"
+import "./framework/frame"
+import "./plugins/plugins"
+
+import "components/card/card"
+
+/*
+    This file can be used as entry point for webpack!
+ */
